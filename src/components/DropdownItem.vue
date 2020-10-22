5ca2107733d7917e7de9a3809373e0058782ca27
name <1521341750@qq.com>
2020-10-22 17:00:56 +0800

    下拉组件

---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="dropdown-option" :class="{'is-disabled':disabled}">
+    <slot></slot>
+  </li>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: '',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
+<style>
+.dropdown-option.is-disabled * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color:transparent ;
+}
+</style>
