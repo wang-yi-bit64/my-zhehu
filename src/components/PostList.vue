1cba5d7649d53ec4fa88dde1c24cab9284b1e5f8
DESKTOP-ENUSKP0\ThinkPad <18538087860@163.com>
2020-12-08 14:04:04 +0800

    修改接口 验证

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-07 16:09:53
+ * @LastEditTime: 2020-12-08 14:03:11
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-zhehu\src\components\PostList.vue
+-->
 <template>
   <div class="post-list">
     <article v-for="post in posts" :key="post.columnId" class="card mb-3 shadow0sm">
@@ -6,10 +14,10 @@
           <router-link to="/">{{ post.title }}</router-link>
         </h4>
         <div class="row my-3 aligin-items-center">
-          <div class="col-4">
+          <div class="col-4" v-if="post.image">
             <img class="rounded-lg w-100" :src="post.image" :alt="post.title" />
           </div>
-          <p :class="{ 'col-8': post.image }" class="text-muted"></p>
+          <p :class="{ 'col-8': post.image }" class="text-muted">{{ post.content }}</p>
         </div>
         <span class="text-muted">{{ post.createdAt }}</span>
       </div>
@@ -18,7 +26,7 @@
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-import { PostProps } from "@/mock/testData";
+import { PostProps } from "@/store";
 export default defineComponent({
   name: "",
   props: {
