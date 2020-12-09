c9ba0528611ab6c42eb77430a6eda4254ac7b381
DESKTOP-ENUSKP0\ThinkPad <18538087860@163.com>
2020-12-09 13:43:05 +0800

    注释本地的假数据

--- src/mock/testData.ts
+++ src/mock/testData.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-07 14:02:52
- * @LastEditTime: 2020-12-08 13:36:59
+ * @LastEditTime: 2020-12-09 11:06:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \my-zhehu\src\mock\testData.ts
@@ -10,46 +10,46 @@
 import {ColumnProps, PostProps} from '@/store'
 
 export const testData:ColumnProps[] = [
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏",
-    avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是test2的专栏",
-    avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
-  },
-  {
-    id: 3,
-    title: "test3的专栏",
-    description: "这是test3的专栏",
-    avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
-  },
-  {
-    id: 4,
-    title: "test1的专栏",
-    description: "这是test1的专栏",
-  },
+  // {
+  //   id: 1,
+  //   title: "test1的专栏",
+  //   description: "这是test1的专栏",
+  //   avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
+  // },
+  // {
+  //   id: 2,
+  //   title: "test2的专栏",
+  //   description: "这是test2的专栏",
+  //   avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
+  // },
+  // {
+  //   id: 3,
+  //   title: "test3的专栏",
+  //   description: "这是test3的专栏",
+  //   avatar: "http://n.sinaimg.cn/photo/crawl/300/w550h550/20200921/282e-izmihnt8087611.jpg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "test1的专栏",
+  //   description: "这是test1的专栏",
+  // },
 ]
 
 export const testPosts:PostProps[] = [
-  {
-    id: 1,
-    title: '这是我的第一篇文章',
-    content: "美国新冠肺炎超1470万例",
-    image: "https://i0.hdslb.com/bfs/archive/622017dd4b0140432962d3ce0c6db99d77d2e937.png",
-    createdAt:'2020-06-11 10:24:22',
-    columnId: 1
-  },
-  {
-    id: 2,
-    title: '这是我的第一篇文章',
-    content: "美国新冠肺炎超1470万例",
-    image: "https://i0.hdslb.com/bfs/archive/622017dd4b0140432962d3ce0c6db99d77d2e937.png",
-    createdAt:'2020-06-12 10:24:22',
-    columnId: 2
-  }
+  // {
+  //   id: 1,
+  //   title: '这是我的第一篇文章',
+  //   content: "美国新冠肺炎超1470万例",
+  //   image: "https://i0.hdslb.com/bfs/archive/622017dd4b0140432962d3ce0c6db99d77d2e937.png",
+  //   createdAt:'2020-06-11 10:24:22',
+  //   columnId: 1
+  // },
+  // {
+  //   id: 2,
+  //   title: '这是我的第一篇文章',
+  //   content: "美国新冠肺炎超1470万例",
+  //   image: "https://i0.hdslb.com/bfs/archive/622017dd4b0140432962d3ce0c6db99d77d2e937.png",
+  //   createdAt:'2020-06-12 10:24:22',
+  //   columnId: 2
+  // }
 ]
